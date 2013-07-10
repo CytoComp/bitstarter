9abccd5c57f22124f073f910ad2064cefdb7c916
Gerd Moe-Behrens <leukipposinstitute@googlemail.com>
2013-07-10 10:00:10 +0000

    Update index.html

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
+
+//variables from the node.js sample
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//read "index.html" from disk and use that data in place of "Hello World 2!
+var fs = require('fs');
+
+var filecatch = fs.readFileSync('index.html','utf-8');
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+//replace "Hello World 2" with the index.html file
+ // response.send('Hello World2!');
+response.send(filecatch)
 });
 
 var port = process.env.PORT || 5000;
